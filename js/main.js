127dbaed4a29fe1e78149eef5fd45d1efc0c3634
Kamil Sagan <kamilsagan@MacBook-Pro-Kamil.local>
2024-11-20 22:48:49 +0100

    footer  i respo

--- js/main.js
+++ js/main.js
@@ -13,4 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			navLinks.classList.toggle('nav__links__mobile-active');
 		}, 500);
 	});
+
+	const footerBTn = document.getElementById('footerBTN');
+	footerBTn.addEventListener('click', () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	});
 });
